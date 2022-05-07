4861b880f784df2e476768152a798bf6e6072d7d
Aya-Mahmoud-99 <ayamahmoudabdelfatah99@gmail.com>
2022-05-07 14:13:00 +0200

    first commit

---
+++ dataset/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class dataset(models.Model):
+    video_path=models.TextField()
+    age=models.IntegerField()
+    gender=models.CharField(max_length=1)
+    age=models.IntegerField()
+    gender=models.CharField(max_length=1)
+    diagnosedWithADHD=models.BooleanField()
+    responseTime =models.FloatField()
+    correctTimingResponses=models.IntegerField()
+    correctAttentionResponses=models.IntegerField()
+    hyperactivityTarget=models.IntegerField()
+    hyperactivityNonTarget=models.IntegerField()
+    hyperactivityRandom=models.IntegerField()
+    impulsiveResponses=models.IntegerField()
+    omissionErrors=models.IntegerField()
+    totalResponses=models.IntegerField()
+
---
+++ dataset/views.py
@@ -0,0 +1,49 @@
+import csv
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import dataset
+from django.http import JsonResponse
+from .serializers import *
+from rest_framework.request import Request
+import json
+from rest_framework.decorators import api_view
+
+
+from django.core.files.storage import FileSystemStorage
+
+# Create your views here.
+
+def export_dataset(request):
+    response=HttpResponse(content_type='text/csv')
+    writer=csv.writer(response)
+    writer.writerow(['video_path','age','gender','diagnosedWithADHD','responseTime','correctTimingResponses','correctAttentionResponses','hyperactivityTarget','hyperactivityNonTarget','hyperactivityRandom','impulsiveResponses','omissionErrors','totalResponses'])
+    for data in dataset.objects.all().values_list('video_path','age','gender','diagnosedWithADHD','responseTime','correctTimingResponses','correctAttentionResponses','hyperactivityTarget','hyperactivityNonTarget','hyperactivityRandom','impulsiveResponses','omissionErrors','totalResponses'):
+        writer.writerow(data)
+    response['Content-Disposition']='attachment; filename="dataset.csv"'
+    return response
+
+@api_view(["POST"])
+def insert_dataset_record(request):
+    videofile = request.FILES['videoData']
+    fs = FileSystemStorage(location="videos")
+    #body_unicode = request.data.decode('utf-8')
+    print(request.data)
+    #body = json.loads(request.data)
+    print(videofile.name)
+    filename = fs.save(videofile.name, videofile)
+    print("request",request.data)
+    request.data['video_path']=filename
+    if request.method == 'POST':
+        serializer = DatasetSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            serializer.save()
+            data['response'] = "Your order went well"
+            return JsonResponse({"data":data})
+        return JsonResponse({"error":serializer.errors})
+
+def hello(request):
+    return "hello"
+
+
+
